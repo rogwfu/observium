42833931d99a4a081e9a89ae97c8143deb89187c
Roger Seagle <rseagle@cisco.com>
2013-05-08 22:22:25 -0400

    Fixed minitest typos

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -5,13 +5,13 @@ describe 'observium::default' do
   include Helpers::Observium
 
   # Check creation of directories
-  it "creates observium directory"
+  it "creates observium directory" do
     directory(node["observium"]["log_dir"]).must_exist
   end
 
   # Check installation of dependent packages
   %w{snmp graphviz subversion rrdtool fping imagemagick whois mtr-tiny nmap ipmitool}.each do |pkg|
-    it "installed #{pkg}"
+    it "installed #{pkg}" do 
       package("#{pkg}").must_be_installed()
     end
   end
