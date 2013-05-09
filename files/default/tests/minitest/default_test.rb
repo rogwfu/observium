3a2e91f473ae76df6abd194e93d8b20ff9fcec77
Roger Seagle <rseagle@cisco.com>
2013-05-08 22:04:48 -0400

    Added first minitest

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -4,8 +4,15 @@ describe 'observium::default' do
 
   include Helpers::Observium
 
-  # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  # Check creation of directories
+  it "creates observium directory"
+    directory(node["observium"]["log_dir"]).must_exist
   end
 
+  # Check installation of dependent packages
+  %w{libapache2-mod-php5 php5-cli php5-mysql php5-gd php5-snmp php-pear snmp graphviz subversion mysql-server mysql-client rrdtool fping imagemagick whois mtr-tiny nmap ipmitool}.each do |pkg|
+    it "installed #{pkg}"
+      package("#{pkg}").must_be_installed()
+    end
+  end
 end
